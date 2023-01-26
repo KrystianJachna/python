a943fe614e29fcf6b3abc17f3443c258b39f5145
Krystian Jachna <krysti4nape@gmail.com>
2023-01-26 19:16:21 +0100

    operatory matematyczne, konwersja, usuwanie zmiennych

---
+++ lekcja2/main.py
@@ -0,0 +1,21 @@
+print("Kolejność: ")
+print(2 + 2 * 2)
+print("Dzielenie: ")
+print(5/2)
+print(5//2)
+print("Mnożenie:")
+print(2*3)
+print(2**3)
+print("Skrócone:")
+x = 5
+x += 1
+print(x)
+print("Konwersja:")
+a = input("Podaj 1 liczbę: ")
+b = input("Podaj 2 liczbę: ")
+print(float(a)+float(b))
+s = 4
+o = 5
+#del s
+print(str(s) + str(o))
+del s, o, x, a, b
