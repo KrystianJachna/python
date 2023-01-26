7bdc1981f40054dbdc2543347f10f9094e02c4be
Krystian Jachna <krysti4nape@gmail.com>
2023-01-26 21:49:57 +0100

    pętla while, instrukcje skoku (break, continue)

--- lekcja4/main.py
+++ lekcja4/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+i = 0
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+while True:
+    i += 1
+    if i % 2 == 1:
+        continue
+    print(i)
+    if i > 10:
+        break
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Koniec")
