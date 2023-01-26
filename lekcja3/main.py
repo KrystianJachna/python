2c3cf7a49e622b92513136e12be329c617be439f
Krystian Jachna <krysti4nape@gmail.com>
2023-01-26 19:30:22 +0100

    operatory porównania, instrukcje warunkowe

---
+++ lekcja3/main.py
@@ -0,0 +1,24 @@
+#x = True
+#y = False
+#print(x)
+#print(y)
+
+# print(1 == 5)
+# print(5 != 5)
+# print(5 > 5)
+# print(5 >= 5)
+
+if 4 == 5:
+    print("Prawda")
+print("Koniec")
+
+x = 15
+y = 10
+
+if x > y:
+    print("X wieksze od Y")
+elif x < y:
+    print("X mniejsze od Y")
+else:
+    print("X rowne Y")
+
