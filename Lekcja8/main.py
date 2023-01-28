b7a62c14315e44932ceb3c20ab11995d0b374ca8
Krystian Jachna <krysti4nape@gmail.com>
2023-01-28 21:39:56 +0100

    FOR i funkcja range

---
+++ Lekcja8/main.py
@@ -0,0 +1,24 @@
+lista = ["a1", "a2", "a3", "a4"]
+
+i = 0
+
+# while i < len(lista):
+#     print(lista[i])
+#     i += 1
+
+
+# x <- przyjmuje argumenty z kolekcji
+for x in lista:
+    print(x)
+
+# print(list(range(10)))
+#
+# for y in range(10):
+#     print(y)
+
+for z in range(1, 11):
+    print(z)
+
+#a1, an-1, r
+for x in range(1, 11, 2):
+    print(x)
