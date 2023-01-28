bc33077d694fca3d0ad7575250710996c83333ed
Krystian Jachna <krysti4nape@gmail.com>
2023-01-29 00:13:05 +0100

    bibliteki math, random. Sposoby zalaczania bibliotek

---
+++ lekcja12/main.py
@@ -0,0 +1,13 @@
+# import random     <-- cala bibliteka
+from random import randint      # <-- jedna funkcja z biblioteki
+from math import pi
+from math import sqrt as pierwiastek    # alias
+
+#print(random.randint(1, 100))
+# skoro z calej biblioteki (opcja 1) to musze
+# wyszczegolnic z ktorej funkcji korzystam
+
+print(randint(1, 1000))
+print(pi)
+print(pierwiastek(3))
+
